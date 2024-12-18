8406c755ae6c36968a594739fc6828505f540177
rahimcisse <abdulrahimmogtaricisse@gmail.com>
2024-12-18 19:56:07 +0000

    javascript

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let PlayGame = confirm("Shall we play rock, paper, or sissors?");
+if (PlayGame) {
+    let playerChoice=prompt("Please enter rock, paper, or scissors?");
+    if (playerChoice) {
+        let playerOne = playerChoice.trim().toLocaleLowerCase();
+        if (playerOne=="rock" || playerOne=="paper"||playerOne=="scissors") {
+
+            let computerChoice = Math.floor(Math.random() * 3 + 1);
+            let computer = computerChoice == 1 ? "rock" 
+            : computerChoice == 2 ? "paper"
+            : "scissors";
+
+        } else {
+        alert("You didn't enter rock, paper, or scissors.");
+    } 
+    } else {
+      alert("I guess you changed your mind. Maybe next time.")
+    }
+} else {
+    alert("Ok, maybe next time.")
+}
